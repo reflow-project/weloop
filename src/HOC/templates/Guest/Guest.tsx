bf65be019d0a3d755cbcb58ab00b8347412b08df
ivan minutillo <bernini@inventati.org>
2020-06-18 15:32:25 +0200

    improvement guest template

--- src/HOC/templates/Guest/Guest.tsx
+++ src/HOC/templates/Guest/Guest.tsx
@@ -2,6 +2,7 @@ import { MainHeaderHOC } from 'HOC/modules/Header/Header';
 import React, { FC } from 'react';
 import { Guest, Props } from 'ui/templates/guest';
 import { ProvideSideBarContext } from 'HOC/context/SideBar';
+import { SearchBox } from 'HOC/modules/SearchBox/SearchBox';
 
 export interface GuestTemplate {
   withoutHeader?: boolean;
@@ -9,7 +10,8 @@ export interface GuestTemplate {
 export const GuestTemplate: FC<GuestTemplate> = ({ children, withoutHeader = false }) => {
   const HeaderBox = withoutHeader ? undefined : <MainHeaderHOC />;
   const props: Props = {
-    HeaderBox
+    HeaderBox,
+    SearchBox: <SearchBox />
   };
   return (
     <ProvideSideBarContext>
--- src/ui/templates/guest.tsx
+++ src/ui/templates/guest.tsx
@@ -1,20 +1,105 @@
 import React from 'react';
 import { Box } from 'rebass/styled-components';
 import styled from 'ui/themes/styled';
+import { useHistory } from 'react-router';
+import { ChevronLeft } from 'react-feather';
 // import Footer from 'ui/modules/Footer';
+import media from 'styled-media-query';
+import { Link } from 'react-router-dom';
 
 export interface Props {
   HeaderBox?: JSX.Element;
+  SearchBox: JSX.Element;
 }
-export const Guest: React.FC<Props> = ({ children, HeaderBox }) => {
+export const Guest: React.FC<Props> = ({ children, HeaderBox, SearchBox }) => {
+  const history = useHistory();
+
   return (
     <Page>
-      {HeaderBox}
+      <SearchBar>
+        <Icon onClick={() => history.goBack()}>
+          <ChevronLeft size="20" />
+        </Icon>
+        {SearchBox}
+        <Link to="/login">Log in</Link>
+      </SearchBar>
+      <Header>{HeaderBox}</Header>
       <Wrapper>{children}</Wrapper>
       {/* <Footer /> */}
     </Page>
   );
 };
+
+const Icon = styled(Box)`
+  cursor: pointer;
+  height: 40px;
+  width: 40px;
+  min-width: 40px;
+  border-radius: 40px;
+  margin-top: 5px;
+  display: flex;
+  align-items: center;
+  &:hover {
+    background: ${props => props.theme.colors.lighter};
+    svg {
+      stroke: ${props => props.theme.colors.primary};
+    }
+  }
+  svg {
+    stroke: ${props => props.theme.colors.mediumdark};
+    margin: 0 auto;
+  }
+`;
+
+const Header = styled(Box)`
+  ${media.lessThan('medium')`
+    display: none;
+  `}
+`;
+
+const SearchBar = styled(Box)`
+a {
+  height: 40px;
+  line-height: 40px;
+  font-size: 14px;
+  padding: 0 8px;
+  border-radius: 4px;
+  text-decoration: none;
+  background: ${props => props.theme.colors.primary};
+  color: ${props => props.theme.colors.lighter};
+  margin-right: 8px;
+}
+height: 75px;
+line-height: 75px;
+text-align: center;
+background: ${props => props.theme.colors.appInverse}
+border-bottom: ${props => props.theme.colors.border};
+display: flex;
+align-items: center;
+input {
+  width: 100%;
+  font-size: 13px;
+  border-radius: 4px;
+  max-width: 500px;
+  height: 40px;
+  margin: 0;
+  border: 0;
+  background: ${props => props.theme.colors.app};
+}
+.ais-SearchBox {
+  border: 0;
+  height: auto;
+  margin: 0;
+  width: auto;
+  margin: 0;
+  margin-right: 8px;
+  margin-left: 8px;
+  flex:1;
+}
+${media.greaterThan('medium')`
+    display: none;
+  `}
+`;
 
 const Page = styled(Box)`
   height: 100vh;
