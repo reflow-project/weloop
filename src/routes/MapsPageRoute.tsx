29dca1f97de49aaece281625cf271260dd6aa5e4
Kári Yngvason <kari@hugsmidjan.is>
2020-11-07 14:00:32 +0000

    feat: WIP display map with static data on map page

--- src/routes/MapsPageRoute.tsx
+++ src/routes/MapsPageRoute.tsx
@@ -3,21 +3,22 @@
 import React, { FC } from 'react';
 import { RouteComponentProps, RouteProps } from 'react-router-dom';
 import { locationHelper } from './lib/helper';
-import { NotFoundHOC } from 'HOC/pages/not-found/NotFound';
+// import { NotFoundHOC } from 'HOC/pages/not-found/NotFound';
 import { WithSidebarTemplate } from 'HOC/templates/WithSidebar/WithSidebar';
+import { MapPageHOC } from 'HOC/pages/map/MapPage';
 
 interface MapsPageRouter {}
 const MapsPageRouter: FC<RouteComponentProps<MapsPageRouter>> = ({ match }) => {
-  const props = null; // TODO: implement Map HOC
+  // const props = null; // TODO: implement Map HOC
 
-  if (props === null) {
-    return <NotFoundHOC />;
-  }
+  // if (props === null) {
+  //   return <NotFoundHOC />;
+  // }
 
   return (
     <WithSidebarTemplate>
       {/* {TODO: use HOC component} */}
-      Map goes here
+      <MapPageHOC />
     </WithSidebarTemplate>
   );
 };
