51566bc7b41d4e2bb7c0cd7395e6f3406b5b8539
Pavlichenko <NikitaPavlichenko13@>
2021-11-12 22:33:42 +0200

    Revert "150 filter fix build"
    This reverts commit fa7f376681aa3fb3481e5f730ccd4b27a91f961d.

--- src/ui/elements/CustomSelect/index.tsx
+++ src/ui/elements/CustomSelect/index.tsx
@@ -123,9 +123,7 @@ export const CustomSelect: FC<Props> = ({
       isOptionDisabled={(option: any) => option.isDisabled}
       onInputChange={(value: string) => {
         if (!noChange && value.length > 2) {
-          if (onInputChange) {
-            onInputChange(name, value);
-          }
+          onInputChange(name, value);
           setIsOpen(true);
         } else if (!noChange && value.length < 3) {
           setIsOpen(false);
